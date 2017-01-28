90bf1af75953f65fde75cd526fd5d0e0b2cf700f
Lior Amsalem <lior.a@fiverr.com>
2017-01-28 12:20:40 +0200

    remove files

--- stories/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { storiesOf, action, linkTo } from '@kadira/storybook';
-import ComponentOne from '../src/ComponentOne';
-
-storiesOf('Component One Example', module)
-    .add('Example Story', () => (
-        <ComponentOne showApp={linkTo('Button')}/>
-    ));
